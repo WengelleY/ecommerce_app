14d5f92abc1c95b474a4ddb0f2ed7dbd761ca642
Wengelle Yohannes <wengelez@gmail.com>
2026-08-06 23:14:24 +0300

    added the cart provider

--- lib/providers/CartProvider.dart
+++ lib/providers/CartProvider.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import '../models/cart_item.dart';
 
 import '../models/product.dart';
 
 class CartProvider extends ChangeNotifier {
-  List<Product> _cartItems = [];
+  List<CartItem> _cartItems = [];
 
-  List<Product> get cartItems => _cartItems;
+  List<CartItem> get cartItems => _cartItems;
 
   void addToCart(Product product) {
-    _cartItems.add(product);
+    final index = _cartItems.indexWhere(
+      (item) => item.product.id == product.id,
+    );
+
+    if (index != -1) {
+      _cartItems[index].quantity++;
+    } else {
+      _cartItems.add(CartItem(product: product));
+    }
 
     notifyListeners();
   }
 
   void removeFromCart(Product product) {
-    _cartItems.remove(product);
+    _cartItems.removeWhere((item) => item.product.id == product.id);
 
     notifyListeners();
   }
@@ -22,8 +31,8 @@ class CartProvider extends ChangeNotifier {
   double get totalPrice {
     double total = 0;
 
-    for (var product in _cartItems) {
-      total += product.price;
+    for (var item in _cartItems) {
+      total += item.product.price * item.quantity;
     }
 
     return total;
